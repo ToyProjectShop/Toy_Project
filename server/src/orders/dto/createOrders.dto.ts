6f55476690f8575eced5e089a4f97e47df49f7cf
baeksbs3 <baeksbs3@gmail.com>
2023-01-13 17:44:11 +0900

    swagger updated

--- server/src/orders/dto/createOrders.dto.ts
+++ server/src/orders/dto/createOrders.dto.ts
@@ -1,30 +1,65 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Order } from '../orders.entity';
 export class CreateOrdersDto {
+  @ApiProperty({
+    example: '1',
+    description: '주문 개수',
+    required: true,
+  })
   @IsNotEmpty()
   @IsNumber()
   readonly count: number;
 
+  @ApiProperty({
+    example: '10000',
+    description: '판매 가격',
+    required: true,
+  })
   @IsNotEmpty()
   @IsNumber()
   readonly price: number;
-
+  @ApiProperty({
+    example: '서울시 강남구 서초동',
+    description: '도시 주소',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   readonly city: string;
 
+  @ApiProperty({
+    example: '영동대로 89 번지 000빌딩 000호',
+    description: '도로명 상세주소',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   readonly street: string;
 
+  @ApiProperty({
+    example: '12334',
+    description: '우편번호, 집코드',
+    required: true,
+  })
   @IsNotEmpty()
   @IsNumber()
   readonly zipcode: number;
 
+  @ApiProperty({
+    example: '01012345678',
+    description: '전화 번호 - 없이 입력',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   readonly phone: string;
 
+  @ApiProperty({
+    example: '1',
+    description: '상품 id',
+    required: true,
+  })
   @IsNotEmpty()
   @IsNumber()
   readonly item_id: number;
--- server/src/orders/orders.controller.ts
+++ server/src/orders/orders.controller.ts
@@ -1,18 +1,40 @@
-import { Body, Controller, Headers, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Headers, Param, Patch, Post, UseInterceptors } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UndefinedtoNullInterceptor } from '../common/interceptors/undefinedToNull.interceptor';
 import { CreateOrdersDto } from './dto/createOrders.dto';
 import { Order } from './orders.entity';
 import { OrdersService } from './orders.service';
 
+@ApiTags('ORDERS')
+@UseInterceptors(UndefinedtoNullInterceptor)
 @Controller('order')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @ApiOperation({
+    summary: '주문',
+    description: '주문하기, 수량, 배송지 입력',
+  })
+  @ApiResponse({
+    type: CreateOrdersDto,
+    status: 201,
+    description: '주문 성공',
+  })
   @Post()
   async create(@Body() orderDto: CreateOrdersDto): Promise<Order> {
     const result = await this.ordersService.create(orderDto);
     return result;
   }
 
+  @ApiOperation({
+    summary: '주문취소',
+    description: '주문상태 취소로 업데이트 ',
+  })
+  @ApiResponse({
+    type: CreateOrdersDto,
+    status: 201,
+    description: '주문취소 성공',
+  })
   @Patch('cancel/:order_id')
   async update(@Param('order_id') order_id: number): Promise<Order> {
     const result = await this.ordersService.update(order_id);

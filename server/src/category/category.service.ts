380682cae66b59e2264e123a1568060d0d68c46b
baeksbs3 <baeksbs3@gmail.com>
2023-01-18 15:10:35 +0900

    category CRUD added

--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -9,8 +9,25 @@ export class CategoryService {
     @InjectRepository(Category)
     private readonly categoryRepository: Repository<Category>,
   ) {}
-  //카테고리 조회
+
   //카테고리 생성
+  async create(user, category_name): Promise<Category> {
+    const result = await this.categoryRepository.save(category_name);
+    return result;
+  }
+
   //카테고리 수정
+  async update(user, category_id, category_name): Promise<Category> {
+    const result = await this.categoryRepository.findOne({ where: { category_id } });
+    await this.categoryRepository.save(category_name);
+    return result;
+  }
   //카테고리 삭제
+  async deleteCategory(user, category_id) {
+    const result = await this.categoryRepository.findOne({ where: { category_id } });
+    await this.categoryRepository.softDelete({ category_id });
+    return true;
+  }
+
+  //카테고리조회 (검색기능 개발하면 추가)
 }
